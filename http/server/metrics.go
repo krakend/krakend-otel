90d2696f77827574db6d1af31ac2a886957d3861
David Hontecillas <dhontecillas@gmail.com>
2024-12-29 07:59:06 +0100

    add semantic convetion 1.27 to server metrics

--- http/server/metrics.go
+++ http/server/metrics.go
@@ -6,6 +6,7 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	v127 "go.opentelemetry.io/otel/semconv/v1.27.0"
 
 	kotelconfig "github.com/krakend/krakend-otel/config"
 )
@@ -14,14 +15,23 @@ type metricsHTTP struct {
 	fixedAttrs     []attribute.KeyValue
 	fixedAttrsOpts metric.MeasurementOption
 
-	latency metric.Float64Histogram
-	size    metric.Int64Histogram
+	latency metric.Float64Histogram // the time it takes to serve the request
+	size    metric.Int64Histogram   // the response size
 }
 
-func newMetricsHTTP(meter metric.Meter, attrs []attribute.KeyValue) *metricsHTTP {
+type metricsFiller func(metricsHTTP, metric.Meter)
+
+func newMetricsHTTP(meter metric.Meter, attrs []attribute.KeyValue, semconv string) *metricsHTTP {
 	var m metricsHTTP
-	m.latency, _ = meter.Float64Histogram("http.server.duration", kotelconfig.TimeBucketsOpt)
-	m.size, _ = meter.Int64Histogram("http.server.response.size", kotelconfig.SizeBucketsOpt)
+
+	supportedSemConv := map[string]metricsFiller{
+		"1.27": semConv1_27MetricsFiller,
+	}
+	fill := noSemConvMetricsFiller
+	if semConvFiller, ok := supportedSemConv[semconv]; ok {
+		fill = semConvFiller
+	}
+	fill(&m, meter)
 	if len(attrs) > 0 {
 		m.fixedAttrs = make([]attribute.KeyValue, len(attrs))
 		copy(m.fixedAttrs, attrs)
@@ -43,4 +53,21 @@ func (m *metricsHTTP) report(t *tracking, r *http.Request) {
 	dynAttrsOpts := metric.WithAttributes(dynAttrs...)
 	m.latency.Record(t.ctx, t.latencyInSecs, m.fixedAttrsOpts, dynAttrsOpts)
 	m.size.Record(t.ctx, int64(t.responseSize), m.fixedAttrsOpts, dynAttrsOpts)
+}
+
+func noSemConvMetricsFiller(m *metricsHTTP, meter metric.Meter) {
+	m.latency, _ = meter.Float64Histogram("http.server.duration", kotelconfig.TimeBucketsOpt)
+	m.size, _ = meter.Int64Histogram("http.server.response.size", kotelconfig.SizeBucketsOpt)
+}
+
+func semConv1_27MetricsFiller() {
+	m.latency, _ = meter.Float64Histogram(v127.HTTPServerRequestDurationName,
+		metric.WithUnit(v127.HTTPServerRequestDurationUnit),
+		metric.WithDescription(v127.HTTPServerRequestDurationDescription),
+		kotelconfig.TimeBucketsOpt)
+
+	m.size, _ = meter.Int64Histogram(v127.HTTPServerResponseBodySizeName,
+		metric.WithUnit(v127.HTTPServerResponseBodySizeUnit),
+		metric.WithDescription(v127.HTTPServerResponseBodySizeDescription),
+		kotelconfig.SizeBucketsOpt)
 }
